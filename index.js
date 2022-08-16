d864480e09f33aee51ba887428a899c140e34a39
Marcus Otterstad <inet2@otterstad.name>
2022-08-16 16:22:46 +0200

    made API front-end accessible

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 4000
+const pool = require('./src/configs/DB.config');
 const {simpleGetRequest} = require('./src/utils/dbHelpers.utils');
+var cors = require('cors')
+
+
+//middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 
 // Routers
 const habitLogRouter = require('./src/routes/habitLogRouter');
-app.use('/habits', habitLogRouter);
-
-
-
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+app.use('/habitlog', habitLogRouter);
 
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.get('/habitlog', simpleGetRequest('SELECT * FROM habit_log;'));
+app.post('/habits/post', (request, response) => {
+    console.log(request);
+    const {date, habit_id, user_id, duration, notes} = request.body;
+    console.log(date, habit_id, user_id, duration, notes);
+  pool.query('INSERT INTO habit_log (date, habit_id, user_id, duration, notes) VALUES ($1, $2, $3, $4, $5) RETURNING *', [date, habit_id, user_id, duration, notes], (error, results) => {
+    if (error) {
+        throw error
+    }
+    response.status(201).send(`Habit number ${results.rows[0].id} added. Well done!`)
+  })
+});
+app.get('/habits', simpleGetRequest('SELECT * FROM habit_info;'));
 
   
-app.listen(port, () => {
+app.listen(port, "127.0.0.1", () => {
     console.log(`App running on port ${port}.`)
 })
--- src/routes/habitLogRouter.js
+++ src/routes/habitLogRouter.js
@@ -6,24 +6,13 @@ const pool = require('../configs/DB.config');
 const habitLogRouter = express.Router();
 
 habitLogRouter.get('/', (request, response) => {
-    const {userId} = request.params;
-    pool.query('SELECT * FROM habit_log WHERE user_id = $1;', [userId], (error, results) => {
+    pool.query('SELECT * FROM habit_log;', (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).json(results.rows)
+      response.status(200).json(results.rows);
     })
   });
 
-habitLogRouter.post('/', (request, response) => {
-    const {date, habit_id, user_id, duration, notes} = request.body;
-
-    pool.query('INSERT INTO habit_log (date, habit_id, user_id, duration, notes) VALUES ($1, $2, $3, $4, $5) RETURNING *', [date, habit_id, user_id, duration, notes], (error, results) => {
-    if (error) {
-        throw error
-    }
-    response.status(201).send(`Habit number ${results.rows[0].id} added.`)
-  })
-});
 
 module.exports = habitLogRouter;
